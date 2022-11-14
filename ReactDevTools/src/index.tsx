60d4859f52a17b9c1d0c79c1e9af6711eb28dd91
Lucas Wilson <lucaswilson4502@outlook.com>
2022-11-14 19:44:55 +0000

    Update to use latest connect function & fixes

--- ReactDevTools/src/index.tsx
+++ ReactDevTools/src/index.tsx
@@ -1,32 +1,44 @@
-import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { React, Toasts } from "enmity/metro/common";
-import Settings from "./components/Settings";
-import manifest from "../manifest.json";
-import { get } from "enmity/api/settings";
-import { getIDByName } from "enmity/api/assets";
+import { Plugin, registerPlugin } from "enmity/managers/plugins"
+import { React, Toasts } from "enmity/metro/common"
+import Settings from "./components/Settings"
+import manifest from "../manifest.json"
+import { get } from "enmity/api/settings"
+import { getIDByName } from "enmity/api/assets"
 
 const ReactDevTools: Plugin = {
-	...manifest,
+	...manifest, // Load manifest information into plugin
 
 	onStart() {
-		if (get("ReactDevTools", "autoconnect")) {
-			let code = `window.connectToDevTools({ host: "${get(
+		if (get("ReactDevTools", "autoConnect")) { // If auto connect is saved as true
+			let host: string = get( // Get saved host and port values
 				"ReactDevTools",
 				"host"
-			)}", port: "${get("ReactDevTools", "port")}" })`;
-			Toasts.open({
-				content: `Connecting to ${get("ReactDevTools", "host")}`,
-				source: getIDByName("check"),
-			});
-			eval(code);
+			) as string, port: string = get(
+				"ReactDevTools",
+				"port"
+			) as string
+			if (host == undefined || port == undefined) {
+				Toasts.open({
+					content: "You have auto connect turned on but haven't set a valid host or port!",
+					source: getIDByName("Small")
+				})
+			} else {
+				Toasts.open({ // Notify user of connection attempt
+					content: `Connecting to ${host}:${port}`,
+					source: getIDByName("check"),
+				})
+				window.connectToDevTools({ host: `${host}`, port: `${port}` }) // Connect to saved host and port
+			}
 		}
 	},
 
-	onStop() {},
+	onStop() {
+		// Something should probably go here but idk what...
+	},
 
 	getSettingsPanel({ settings }) {
-		return <Settings settings={settings} />;
+		return <Settings settings={settings} />
 	},
-};
+}
 
-registerPlugin(ReactDevTools);
+registerPlugin(ReactDevTools)
